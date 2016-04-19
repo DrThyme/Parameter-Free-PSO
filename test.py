12d0bf7d3e150358a848f89440975fed14112e8e
DrThyme <jozefusan.chimu@gmail.com>
2016-04-19 11:10:31 +0000

    added both parameter free and normal to tests

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from pso import pso
+from parafreePSO import pso as pfpso
 import math
 
 def banana(x):
@@ -21,8 +22,21 @@ def con(x):
     x2 = x[1]
     return [-(x1 + 0.25)**2 + 0.75*x2]
 
-lb = [-3, -1]
-ub = [2, 6]
+lb = [-32, -32, -32, -32, -32, -32]
+ub = [32, 32, 32, 32, 32, 32]
 
+print("###################################################")
+print("Normal PSO")
+print("###################################################")
 xopt, fopt, p, fp = pso(ack, lb, ub, particle_output=True)
 print xopt, fopt, p, fp
+print("###################################################")
+print("Parameter Free PSO")
+print("###################################################")
+xoptpf, foptpf, p, fp = pfpso(ack, lb, ub, particle_output=True)
+print xoptpf, foptpf, p, fp
+print("###################################################")
+print("Normal PSO")
+print xopt, fopt
+print("PF PSO")
+print xoptpf, foptpf
