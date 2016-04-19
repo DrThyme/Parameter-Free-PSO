0ef30574b457cb6050da07d13f7c31a93bb33d8c
Octave Mariotti <octave.mariotti@ens-lyon.fr>
2016-04-19 12:56:12 +0200

    First tests.

--- test.py
+++ test.py
@@ -1,10 +1,21 @@
 from pso import pso
+import math
 
 def banana(x):
     x1 = x[0]
     x2 = x[1]
     return x1**4 - 2*x2*x1**2 + x2**2 + x1**2 - 2*x1 + 5
-
+    
+def ack(chromosome):
+    firstSum = 0.0
+    secondSum = 0.0
+    for c in chromosome:
+        firstSum += c**2.0
+        secondSum += math.cos(2.0*math.pi*c)
+    n = float(len(chromosome))
+    return -20.0*math.exp(-0.2*math.sqrt(firstSum/n)) - math.exp(secondSum/n) + 20 + math.e
+	
+	
 def con(x):
     x1 = x[0]
     x2 = x[1]
@@ -13,5 +24,5 @@ def con(x):
 lb = [-3, -1]
 ub = [2, 6]
 
-xopt, fopt = pso(banana, lb, ub, f_ieqcons=con)
-print xopt, fopt
+xopt, fopt, p, fp = pso(ack, lb, ub, particle_output=True)
+print xopt, fopt, p, fp
